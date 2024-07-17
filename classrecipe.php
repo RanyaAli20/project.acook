6d53f332976a776b0200327b81be4e2df23a2825
daniabrebishhh <156687688+daniabrebishhh@users.noreply.github.com>
2024-07-17 23:39:58 +0200

    Create classrecipe.php
    كلاس الوصفة

---
+++ classrecipe.php
@@ -0,0 +1,19 @@
+<?php
+class Recipe {
+    private $id;
+    private $content;
+
+    public function __construct($id, $content) {
+        $this->id = $id;
+        $this->content = $content;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+}
+?>
