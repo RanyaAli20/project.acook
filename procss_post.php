91ece3d0bbe5df5373eca4bc16864eac09330f39
RanyaAli20 <ranyaali.2000918@gmail.com>
2024-07-18 05:34:19 +0200

    inshapost

--- procss_post.php
+++ procss_post.php
@@ -1,5 +1,21 @@
 <?php
+session_start();
+
+require_once 'Database.php';
+require_once 'Post.php';
+require_once 'Recipe.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['title'], $_POST['ingredients'], $_POST['recipe'], $_POST['mealType'], $_POST['country'])) {
+    $userId = $_SESSION['user_id'];
+    $title = $_POST['title'];
+    $ingredients = $_POST['ingredients'];
+    $recipeContent = $_POST['recipe'];
+    $mealType = $_POST['mealType'];
+    $country = $_POST['country'];
 
 
 
+
+
+}
 ?>
